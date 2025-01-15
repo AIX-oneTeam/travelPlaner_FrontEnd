17d57697cbb577f3df4c2fa8fde0d2d2318ff022
= <=>
2025-01-15 13:02:35 +0900

    fix: [기능] #2 프로젝트 디렉토리 구조 변경

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // 전역 스타일 및 공통 레이아웃 컴포넌트
 import "./assets/css/All.css";
-import Header from "./pages/include/Header";
-import Footer from "./pages/include/Footer";
+import Header from "./component/header/Header";
+import Footer from "./component/footer/Footer";
 
 import WebTest from "./pages/main/WebTest"; // 메인화면면
 import LoginForm from "./pages/user/LoginForm"; // 로그인폼
