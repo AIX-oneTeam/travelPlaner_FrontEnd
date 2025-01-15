4eaf5ef6daddedbffd45c036af590d871938d4ad
H-JUYEONG <dud9902@gmail.com>
2025-01-15 22:05:24 +0900

    Add: [기능] #7 타입스크립트 세팅

---
+++ src/pages/main/WebTest.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>TypeScript Test</h1>
+      <p>Welcome to the Home Page!</p>
+    </div>
+  );
+};
+
+export default Home;
