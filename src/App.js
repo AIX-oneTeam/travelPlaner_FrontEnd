c1ccdf1616a944fa6871d0c5de62b5f5832b7838
황주영 <dud9902@gmail.com>
2025-01-15 18:02:46 +0900

    Add: [기능] #1 로그인폼html/css 추가 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // 전역 스타일 및 공통 레이아웃 컴포넌트
-
 import "./assets/css/common/variables.css";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
-import WebTest from "./pages/main/WebTest"; // 메인화면면
+import WebTest from "./pages/main/WebTest"; // 메인화면
 import LoginForm from "./pages/user/LoginForm"; // 로그인폼
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<home />} />
+          <Route path="/" element={<WebTest />} />
           <Route path="/loginform" element={<LoginForm />} />
         </Routes>
         <Footer />
--- src/pages/user/LoginForm.jsx
+++ src/pages/user/LoginForm.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./LoginForm.css";
 
 const LoginForm = () => {
+  
   // 일반 메소드 (로그인 이벤트 핸들러)
   const handleKakaoLogin = () => {
     console.log("카카오 로그인 클릭");
@@ -16,44 +17,45 @@ const LoginForm = () => {
   const handleGoogleLogin = () => {
     console.log("Google 로그인 클릭");
     // Google 로그인 로직 추가
+  };
 
-    return (
-      <div id="login-container">
-        <div className="login-form">
-          <div className="logo-placeholder">예비로고</div>
-          <div className="login-prompt">
-            로그인하고 나만의 여행을
-            <br />
-            시작해 보세요
-          </div>
+  return (
+    <div id="login-container">
+      <div className="login-form">
+        <div className="logo-placeholder">예비로고</div>
+        <div className="login-prompt">
+          로그인하고 나만의 여행을
+          <br />
+          시작해 보세요
+        </div>
 
-          {/* 카카오 로그인 */}
-          <div className="kakao-login-button">
-            <img
-              src="/images/kakao_login_btn.png"
-              alt="카카오 로그인 버튼"
-              onClick={handleKakaoLogin}
-            />
-          </div>
+        {/* 카카오 로그인 */}
+        <div className="kakao-login-button">
+          <img
+            src="/images/kakao_login_btn.png"
+            alt="카카오 로그인 버튼"
+            onClick={handleKakaoLogin}
+          />
+        </div>
 
-          {/* 네이버 로그인 */}
-          <div className="naver-login-button">
-            <button onClick={handleNaverLogin}>
-              <img src="/images/btnG_아이콘사각.png" alt="네이버" />
-              <span>네이버 로그인</span>
-            </button>
-          </div>
+        {/* 네이버 로그인 */}
+        <div className="naver-login-button">
+          <button onClick={handleNaverLogin}>
+            <img src="/images/naver-logo.png" alt="네이버" />
+            <span>네이버 로그인</span>
+          </button>
+        </div>
 
-          {/* 구글 로그인 */}
-          <div className="google-login-button">
-            <button onClick={handleGoogleLogin}>
-              <img src="/images/google-logo.png" alt="구글" />
-              <span>Sign in with Google</span>
-            </button>
-          </div>
+        {/* 구글 로그인 */}
+        <div className="google-login-button">
+          <button onClick={handleGoogleLogin}>
+            <img src="/images/google-logo.png" alt="구글" />
+            <span>Google 계정으로 로그인</span>
+          </button>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 };
+
 export default LoginForm;
