55e3c49926718d97fa6770173fd7dcf030bfb80c
= <=>
2025-01-16 13:22:02 +0900

    Add [기능] #5 inputBox 구현

--- src/pages/main/HjWebTest.tsx
+++ src/pages/main/HjWebTest.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import LongBtn from "../../components/buttons/LongBtn";
 import ShortBtn from "../../components/buttons/ShortBtn";
 import RadioBtn from "../../components/buttons/RadioBtn";
+import NormalInput from "../../components/input/NormalInput";
+import SearchInput from "../../components/input/SearchInput";
 
 const Home: React.FC = () => {
   return (
@@ -9,6 +11,8 @@ const Home: React.FC = () => {
       <LongBtn />
       <ShortBtn />
       <RadioBtn />
+      <NormalInput />
+      <SearchInput />
     </div>
   );
 };
