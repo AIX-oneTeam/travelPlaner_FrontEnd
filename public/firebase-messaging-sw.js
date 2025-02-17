e1e04e1198ad8a13926b860bcc5704908099eea0
Jomalang <rhkddlr98@naver.com>
2025-02-17 21:07:40 +0900

    Add: [기능] 백그라운드 알림 기능 추가

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,41 @@
+// eslint-disable-next-line no-undef
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js"
+);
+// eslint-disable-next-line no-undef
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js"
+);
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FCM_API_KEY,
+  authDomain: process.env.REACT_APP_FCM_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FCM_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FCM_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FCM_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FCM_APP_ID,
+  measurementId: process.env.REACT_APP_FCM_MEASUREMENT_ID,
+};
+
+// Firebase 초기화
+// eslint-disable-next-line no-undef
+firebase.initializeApp(firebaseConfig);
+// eslint-disable-next-line no-undef
+const messaging = firebase.messaging();
+
+// 백그라운드 푸시 알림 처리
+messaging.onBackgroundMessage((payload) => {
+  console.log("백그라운드 메시지 수신:", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/logo192.png",
+  };
+
+  // eslint-disable-next-line no-restricted-globals
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
 /* eslint-disable no-restricted-globals */
 self.addEventListener("install", function (e) {
   console.log("fcm sw install..");
@@ -18,6 +56,7 @@ self.addEventListener("push", function (e) {
     body: resultData.body,
     icon: resultData.image,
     tag: resultData.tag,
+    vibrate: [200, 100, 200, 100],
     ...resultData,
   };
   console.log("push: ", { resultData, notificationTitle, notificationOptions });
@@ -27,8 +66,38 @@ self.addEventListener("push", function (e) {
 
 self.addEventListener("notificationclick", function (event) {
   console.log("notification click");
-  const url = "https://easyTravel.jomalang.com";
+  event.preventDefault();
   event.notification.close();
+
+  const url = "https://easyTravel.jomalang/plans";
+
+  //클라이언트에 사이트가 열러있는지 확인
   // eslint-disable-next-line no-undef
-  event.waitUntil(clients.openWindow(url));
+  const promiseChain = clients
+    .matchAll({
+      type: "window",
+      includeUncontrolled: true,
+    })
+    .then(function (windowClients) {
+      let matchingClient = null;
+
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        if (windowClient.url.includes(url)) {
+          matchingClient = windowClient;
+          break;
+        }
+      }
+
+      // 열려있다면 focus, 아니면 새로 open
+      if (matchingClient) {
+        return matchingClient.focus();
+      } else {
+        // eslint-disable-next-line no-undef
+        return clients.openWindow(url);
+      }
+    });
+
+  // eslint-disable-next-line no-undef
+  event.waitUntil(promiseChain);
 });
